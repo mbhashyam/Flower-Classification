2e6ed1e3169aefb0a87533cad0a0e65cbd0195bd
mbhashyam <mbhashyam.ms@gmail.com>
2024-10-14 16:57:31 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
 import keras
 from keras.models import load_model
-import streamlit as st 
+import streamlit as st
 import tensorflow as tf
 import numpy as np
 import base64
 st.header(":black[Flower Classification Model]")
 flower_names = ['Daisy', 'Dandelion', 'Rose', 'Sunflower', 'Tulip']
 
-model = load_model('/Users/anushakurra/Flower_recog_Model/Flower_Recog_Model.h5')
+model = load_model(
+    '/Users/mbhashyam/Desktop/Project/Flower_recog_Model/Flower_Recog_Model.h5')
+
+
 def set_bg_hack(main_bg):
     '''
     A function to unpack an image from root folder and set as bg.
@@ -20,23 +23,25 @@ def set_bg_hack(main_bg):
 
     st.markdown(
         f"""
-         <style>
-         .stApp {{
-             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
-             background-size: cover
-         }}
-         </style>
-         """,
+        <style>
+        .stApp {{
+            background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+            background-size: cover
+        }}
+        </style>
+        """,
         unsafe_allow_html=True
     )
+
 
 side_bg = 'bg4.avif'
 set_bg_hack(side_bg)
 
+
 def classify_images(image_path):
-    input_image = tf.keras.utils.load_img(image_path, target_size=(180,180))
+    input_image = tf.keras.utils.load_img(image_path, target_size=(180, 180))
     input_image_array = tf.keras.utils.img_to_array(input_image)
-    input_image_exp_dim = tf.expand_dims(input_image_array,0)
+    input_image_exp_dim = tf.expand_dims(input_image_array, 0)
 
     predictions = model.predict(input_image_exp_dim)
     result = tf.nn.softmax(predictions[0])
@@ -54,13 +59,12 @@ def classify_images(image_path):
     return outcome
 
 
-
 uploaded_file = st.file_uploader('Upload an Image')
 if uploaded_file is not None:
     with open(os.path.join('upload', uploaded_file.name), 'wb') as f:
         f.write(uploaded_file.getbuffer())
 
-    st.image(uploaded_file, width = 200)
+    st.image(uploaded_file, width=200)
 
     # st.markdown(classify_images(uploaded_file))
 
